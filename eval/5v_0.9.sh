96627c20bab738eae198eb384f0202cac2d61b01
shiyue001 <48040941+shiyue001@users.noreply.github.com>
2025-01-16 04:05:17 +0800

    Update 5v_0.9.sh

--- eval/5v_0.9.sh
+++ eval/5v_0.9.sh
@@ -6,4 +6,4 @@
 
 unset PYTHONPATH 
 source /cluster/project/cvl/yueshi/env/ibr1/bin/activate
-python eval.py --config ../configs/eval_dtu.txt --ckpt_path /cluster/project/cvl/yueshi/code/IBR_vsr/results/dtu_depth0.9_5views/model_250000.pth --num_source_views 5 --expname dtu_5v_0.9
+python eval.py --config ../configs/eval_dtu.txt --ckpt_path ../code/IBR_vsr/results/dtu_depth0.9_5views/model_250000.pth --num_source_views 5 --expname dtu_5v_0.9
